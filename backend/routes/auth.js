21562aa1884df1fb4b222d092d32f6571cfbaa6a
TarzqnDev <mjjjantework@gmail.com>
2025-10-29 22:41:40 +0800

    Added Login Function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+
     try {
-        const { firstName, lastName, email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
         if (existingUser) {
@@ -25,6 +26,34 @@ router.post('/register', async (req, res) => {
 
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: 'Error' });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'User Not Found'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Incorrect Password'});
+        }
+
+        res.status(200).json({ message: 'Login Successful', user: {
+            id: user._id,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
+        }});
+        
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ message: 'Error' });
     }
 });
--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,7 +1,66 @@
+'use client'
+
+import Button from "@/app/components/button";
+import InputField from "@/app/components/inputField";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function LoginPage(){
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await axios.post('http://192.168.0.23:5000/api/auth/login', {
+            email, password
+        });
+
+        router.push('/dashboard');
+    }
+
     return(
-        <>
-            <h1>Login</h1>
-        </>
+        <div className="flex h-screen">
+            <div className="w-3/5 bg-(--light-gray-shade)">
+
+            </div>
+
+            <div className="w-2/5 flex justify-center">
+                <div className="w-3/5 mt-20">
+                    <div className="mb-8">
+                         <h1 className="font-bold text-3xl">LOGIN TO OLYMFIT</h1>
+                         <span>Welcome back! Lets crush your fitness goals together</span>
+                    </div>
+
+                    <form onSubmit={handleLogin}>
+                        <InputField 
+                            type="email"
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+
+                        <InputField 
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+
+                        <p className="text-center mb-4">
+                             <Link href="/" className="font-bold underline">Forgot Password?</Link>
+                        </p>
+
+                        <Button type="submit">
+                            LOGIN
+                        </Button>
+                    </form>
+
+                    <p className="text-center mt-4">Dont have an account? <Link href="/auth/register" className="font-bold underline">Register</Link></p>
+                </div>
+            </div>
+        </div>
     )
 }
